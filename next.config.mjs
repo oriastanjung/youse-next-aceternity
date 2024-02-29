02c0718fcd42ef936751da15e0e00417c2df559b
oriastanjung <oriastan999@gmail.com>
2024-02-29 21:23:37 +0700

    finish (NOT RESPONSIVE)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'assets.aceternity.com',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'aceternity.com',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'user-images.githubusercontent.com',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'code.visualstudio.com',
+            port: '',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
